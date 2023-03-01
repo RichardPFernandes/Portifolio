70279463afb85f41982e369fcca896dd96130f8c
Richard Fernandes <fernandesrichard312@gmail.com>
2023-02-28 22:03:50 -0300

    1

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,15 +1,28 @@
 class MobileNavbar {
-  constructor(mobileMenu, navBar, navLinks) {
+  constructor(mobileMenu, navbar, navLinks) {
     this.mobileMenu = document.querySelector(mobileMenu);
-    this.navBar = document.querySelector(navBar);
+    this.navbar = document.querySelector(navbar);
     this.navLinks = document.querySelectorAll(navLinks);
     this.activeClass = "active";
 
-    this.handleClick = this.handleClick.bind(this);
+
+this.handleClick = this.handleClick.bind(this);
+    
   }
-  handeClick() {
-    console.log(this);
-    this.navBar.classList.toggle(this.activeClass);
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+  handleClick() {
+    this.navbar.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  
   }
   addClickEvent() {
     this.mobileMenu.addEventListener("click", this.handleClick);
